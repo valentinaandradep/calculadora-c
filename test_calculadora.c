f230401f93bc0abf92bcbc3b6178dbcd8d0165b6
valentinaandradep <valentinaandradedepedro1@gmail.com>
2026-05-06 18:59:51 -0300

    Update test_calculadora.c

--- test_calculadora.c
+++ test_calculadora.c
@@ -6,6 +6,6 @@ void main()
 {
     assert(multiplicar(2,0)==0);
     assert(multiplicar(3,6)==18);
-    assert(multiplicar(42,64));
+    assert(multiplicar(42,64)==2688);
     printf("Teste executado com sucesso");
 }
